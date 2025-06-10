3448dffee6c79536d433ecb825a43940b8f8511d
Mateo Espinosa <mateoespinosam@hotmail.com>
2025-06-10 18:10:08 -0500

    Version 1.5

--- Back/Login/Read_User/Services/notification.service.js
+++ Back/Login/Read_User/Services/notification.service.js
@@ -1,19 +1,34 @@
+// export class NotificationService {
+//     constructor(socket) {
+//         this.socket = socket;
+//     }
+
+//     async notify(userId, status, message) {
+//         try {        
+//             this.socket.emit('Profile', {
+//                 Id: userId + "_Read_User",
+//                 Status: status,
+//                 Message: message
+//             });        
+//         } catch (error) {
+//             console.log("Communication server error");
+//         }
+//     }
+// }
+
+
 export class NotificationService {
     constructor(socket) {
         this.socket = socket;
     }
 
     async notify(userId, status, message) {
-        try {        
-
-            // Unirse a la sala correspondiente al usuario
-            this.socket.emit("joinRoom", userId + "_Read_User");
-
-            this.socket.emit('Profile', {
-                Id: userId + "_Read_User",
+        try {
+            this.socket.emit("Profile", {
+                Id: userId + "_Update_User",
                 Status: status,
                 Message: message
-            });        
+            });
         } catch (error) {
             console.log("Communication server error");
         }
