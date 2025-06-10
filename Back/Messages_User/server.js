60e8e7204c7ab366eaa2e0e9151d59b958c06eb0
Mateo Espinosa <mateoespinosam@hotmail.com>
2025-06-10 17:50:57 -0500

    Version 1.5

--- Back/Messages_User/server.js
+++ Back/Messages_User/server.js
@@ -11,8 +11,10 @@ const server = createServer(app);
 const io = new Server(server, {
     maxHttpBufferSize: 50 * 1024 * 1024, // 5 MB limit for messages (adjust according to needs)
     cors: {
+        origin: ["*"], 
         methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true
     }
 });        
 
