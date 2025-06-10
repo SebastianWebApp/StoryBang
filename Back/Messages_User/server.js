bbd580bb77b55d649db7b2bfbfea687c0c615704
Mateo Espinosa <mateoespinosam@hotmail.com>
2025-06-10 16:12:38 -0500

    Version 1.4

--- Back/Messages_User/server.js
+++ Back/Messages_User/server.js
@@ -20,9 +20,10 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     // Unirse a una sala
     socket.on("joinRoom", (room) => socket.join(room));
-
+    console.log(`User connected: ${socket.id}`);
     // Enviar mensaje a una sala
     socket.on("Profile", ({ Id, Message, Status }) => {
+        console.log(`Profile event received for Id: ${Id}, Status: ${Message}`);
         socket.broadcast.to(Id).emit("Profile_Response", { Message, Status });
     });
 });
