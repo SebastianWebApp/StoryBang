198fc15c5f6889f61f39ee83977f94f43e45a200
Mateo Espinosa <mateoespinosam@hotmail.com>
2025-07-08 16:08:55 -0500

    feat: :fire: Front en HTTPS

--- Frontend/server.js
+++ Frontend/server.js
@@ -11,6 +11,10 @@ import router_generator from "./Routers/routers_generator.js";
 import router_character from "./Routers/routers_character.js";
 import router_story from "./Routers/routers_story.js";
 
+import https from "https";
+import fs from "fs";
+
+
 // Enable connection with the .env file
 dotenv.config();
 const PORT = process.env.PORT;
@@ -18,6 +22,12 @@ const PORT = process.env.PORT;
 // Get the path of the elements
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+
+const privateKey = fs.readFileSync(path.join(__dirname, "private.pem"), "utf8");
+const certificate = fs.readFileSync(path.join(__dirname, "public.pem"), "utf8");
+const credentials = { key: privateKey, cert: certificate };
+
 
 // Initialize express
 const app = express();
@@ -108,7 +118,12 @@ app.use((req, res) => {
     }
 });
 
-// Start Server
-app.listen(PORT, () => {
-    console.log(`Server Active http://localhost:${PORT}`);
+// // Start Server
+// app.listen(PORT, () => {
+//     console.log(`Server Active http://localhost:${PORT}`);
+// });
+
+// Start HTTPS Server
+https.createServer(credentials, app).listen(PORT, () => {
+    console.log(`HTTPS Server Active at https://localhost:${PORT}`);
 });
