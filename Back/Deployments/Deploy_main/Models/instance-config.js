37f25cc5ae376f3698fc571c8b1e19c0e49fa472
Mateo Espinosa <94413681+SebastianWebApp@users.noreply.github.com>
2025-07-18 07:59:45 -0500

    feat: :zap: Deploy

--- Back/Deployments/Deploy_main/Models/instance-config.js
+++ Back/Deployments/Deploy_main/Models/instance-config.js
@@ -1382,7 +1382,7 @@ export default InstanceConfig;
 // EC2_USER="ubuntu"
 // EC2_HOST="ec2-54-235-74-45.compute-1.amazonaws.com"
 // LOCAL_PORT=8080
-// REMOTE_IP="172.31.87.246"
+// REMOTE_IP="172.31.91.34"
 // REMOTE_PORT=4020
 
 // # ----------------------------
